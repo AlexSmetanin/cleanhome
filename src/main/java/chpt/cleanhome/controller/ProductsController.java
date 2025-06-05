6e4b90fed5bd1f2d1573c0cb1da385b6d0e0c1ac
AlexSmetanin <AlexSmetanin72@gmail.com>
2025-06-06 02:18:19 +0300

    Дороблено форму додавання товарів

--- src/main/java/chpt/cleanhome/controller/ProductsController.java
+++ src/main/java/chpt/cleanhome/controller/ProductsController.java
@@ -3,13 +3,20 @@ package chpt.cleanhome.controller;
 import chpt.cleanhome.entity.Product;
 import chpt.cleanhome.entity.ProductDto;
 import chpt.cleanhome.repository.ProductsRepository;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.*;
+import java.util.Date;
 import java.util.List;
 
 @Controller
@@ -23,7 +30,7 @@ public class ProductsController {
     public String showProductsList(Model model) {
         List<Product> products = productsRepository.findAll(Sort.by(Sort.Direction.DESC, "id"));
         model.addAttribute("products", products);
-        return "products/productsList";
+        return "products/products";
     }
 
     @GetMapping("/create")
@@ -31,5 +38,79 @@ public class ProductsController {
         ProductDto productDto = new ProductDto();
         model.addAttribute("productDto", productDto);
         return "products/createProduct";
+    }
+
+    @PostMapping("/create")
+    public String createProduct(
+            @Valid @ModelAttribute ProductDto productDto,
+            BindingResult result) {
+        if (productDto.getImageFile().isEmpty()) {
+            result.addError(new FieldError("productDto", "imageFile", "Файл зображення обов'язковий"));
+        }
+
+        if (result.hasErrors()) {
+            return "products/createProduct";
+        }
+
+        // Зберігаємо файл картинки
+        MultipartFile image = productDto.getImageFile();
+        Date createAt = new Date();
+        String storageFileName = createAt.getTime() + "_" + image.getOriginalFilename();
+
+        try {
+            String uploadDir= "public/images";
+            Path uploadPath = Paths.get(uploadDir);
+
+            if (!Files.exists(uploadPath)) {
+                Files.createDirectories(uploadPath);
+            }
+
+            try (InputStream inputStream = image.getInputStream()) {
+                Files.copy(inputStream, Paths.get(uploadDir + storageFileName),
+                        StandardCopyOption.REPLACE_EXISTING);
+            }
+        } catch (IOException ex) {
+            System.out.println("Exception:" + ex.getMessage());
+        }
+
+        Product product = new Product();
+        product.setProductName(productDto.getProductName());
+        product.setBrand(productDto.getBrand());
+        product.setCategory(productDto.getCategory());
+        product.setPrice(productDto.getPrice());
+        product.setQuantity(productDto.getQuantity());
+        product.setDescription(productDto.getDescription());
+        product.setImageFileName(storageFileName);
+
+        productsRepository.save(product);
+
+        return "redirect:/products";
+    }
+
+    @GetMapping("/edit")
+    public String showEditProductForm(
+            Model model,
+            @RequestParam int id) {
+        try {
+            Product product = productsRepository.findById((long) id).get();
+            model.addAttribute("product", product);
+
+            ProductDto productDto = new ProductDto();
+            productDto.setProductName(product.getProductName());
+            productDto.setBrand(product.getBrand());
+            productDto.setCategory(product.getCategory());
+            productDto.setPrice(product.getPrice());
+            productDto.setQuantity(product.getQuantity());
+            productDto.setDescription(product.getDescription());
+
+            model.addAttribute("productDto", productDto);
+        }
+        catch (Exception ex) {
+            System.out.println("Exception:" + ex.getMessage());
+            return "products";
+        }
+
+        return "products/editProduct";
+
     }
 }
--- src/main/java/chpt/cleanhome/repository/ProductsRepository.java
+++ src/main/java/chpt/cleanhome/repository/ProductsRepository.java
@@ -3,5 +3,8 @@ package chpt.cleanhome.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import chpt.cleanhome.entity.Product;
 
+import java.util.List;
+
 public interface ProductsRepository extends JpaRepository<Product, Long> {
+    List<Product> id(Long id);
 }
