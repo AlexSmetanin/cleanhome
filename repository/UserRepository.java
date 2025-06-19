c4b32ce29f773c66746039c0017b5dd71823b701
AlexSmetanin <AlexSmetanin72@gmail.com>
2025-06-20 01:11:09 +0300

    Додавання в список покупок та повідомлення

--- repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package chpt.cleanhome.repository;
-
-import chpt.cleanhome.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
--- src/main/java/chpt/cleanhome/controller/OrderController.java
+++ src/main/java/chpt/cleanhome/controller/OrderController.java
@@ -3,19 +3,24 @@ package chpt.cleanhome.controller;
 import chpt.cleanhome.entity.Order;
 import chpt.cleanhome.entity.Product;
 import chpt.cleanhome.repository.OrderRepository;
+import chpt.cleanhome.repository.ProductsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
 @Controller
 public class OrderController {
+    private List<Product> productsList;
 
     @Autowired
     private OrderRepository orderRepository;
+    private ProductsRepository productsRepository;
 
     @GetMapping({"/orders"})
     public String showOrdersList(Model model) {
@@ -23,4 +28,19 @@ public class OrderController {
         model.addAttribute("orders", orders);
         return "orders/orders";
     }
+
+    @PostMapping({"/orders/addToCart"})
+    public String addProductToCart(
+            Model model,
+            @RequestParam int id) {
+            try {
+                Product product = productsRepository.findById((long) id).get();
+                productsList.add(product);
+            }
+            catch (Exception ex) {
+                System.out.println("Exception:" + ex.getMessage());
+            }
+        return "main";
+    }
+
 }
